af2d59d9f5ca4b13cf8c7466fceaa04a89895b19
Sajal Gupta <sajal07091999@gmail.com>
2020-09-09 01:03:01 +0530

    50. Pow(x, n)

---
+++ myPow.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1.0;
+        long nn=n;
+        if(nn<0) nn*=-1;
+        while(nn>0){
+            if(nn%2==0){
+                x*=x; nn/=2;
+            }
+            else{
+                nn--; ans*=x;
+            }
+        }
+        if(n<0) ans=(double)1.0/ans;
+        return ans;
+    }
+}
