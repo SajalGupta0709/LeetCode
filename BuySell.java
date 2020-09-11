5f74399e3f9cd77b93f3b40b6f8333254a61bc72
Sajal Gupta <sajal07091999@gmail.com>
2020-09-11 23:55:04 +0530

    121. Best Time to Buy and Sell Stock

--- BuySell.java
+++ BuySell.java
@@ -3,7 +3,8 @@ class Solution {
         if(a.length==1||a.length==0) return 0;
         int s=a[0],l=a[0],b=0; 
         for(int i=1;i<a.length;i++){
-            if(a[i]<s){s=a[i];l=a[i];}
+            if(a[i]<s){
+                s=a[i];l=a[i];}
             if(a[i]>s&&l<a[i]) l=a[i];
             if(b<l-s)b=l-s;
         }
