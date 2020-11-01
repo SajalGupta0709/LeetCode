7f077bcb6cd35a5286f927a548f29cf2f638071e
Sajal Gupta <sajal07091999@gmail.com>
2020-11-02 00:58:49 +0530

    May LeetCoding Challenge

--- Ransom Note.java
+++ Ransom Note.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i=0;i<26;i++) fr[i]=0;
         for(int i=0;i<r.length();i++){
             int ch=(int)r.charAt(i)-'a';
-            fr[ch]++;  }
+            fr[ch]+=1;  }
         for(int i=0;i<m.length();i++){
             int ch=(int)m.charAt(i)-'a';
-            fr[ch]--;
+            fr[ch]-=1;
         }
         for(int i=0;i<26;i++) {
             if(fr[i]>0) return false;   
