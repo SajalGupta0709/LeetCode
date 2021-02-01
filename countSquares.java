58b0bcc9f29174adad796cdf34c348cb4978602b
Sajal Gupta <sajal07091999@gmail.com>
2021-02-01 23:56:03 +0530

    1277. Count Square Submatrices with All Ones

--- countSquares.java
+++ countSquares.java
@@ -3,7 +3,6 @@ class Solution {
     int count = 0;
     for(int i = 0; i < matrix.length; i++) 
       for(int j = 0; j < matrix[0].length; j++){ 
-        //Nothing need to be done on 0th column and row ;
           if(matrix[i][j] > 0 && i > 0 && j > 0) 
           matrix[i][j] = Math.min(matrix[i-1][j], Math.min(matrix[i][j-1], matrix[i-1][j-1]))+1;
           count += matrix[i][j];
